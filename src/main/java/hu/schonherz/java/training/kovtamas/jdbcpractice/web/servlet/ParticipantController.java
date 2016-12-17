5df57e69237bd5b9ae6655927cd80308a8db714d
Joey <kovtamas1991@gmail.com>
2016-12-17 11:54:32 +0100

    Converted servlet to controller (ParticipantController).

--- src/main/java/hu/schonherz/java/training/kovtamas/jdbcpractice/web/servlet/ParticipantController.java
+++ src/main/java/hu/schonherz/java/training/kovtamas/jdbcpractice/web/servlet/ParticipantController.java
@@ -1,13 +1,13 @@
 package hu.schonherz.java.training.kovtamas.jdbcpractice.web.servlet;
 
 import hu.schonherz.java.training.kovtamas.jdbcpractice.service.api.ParticipantService;
-import hu.schonherz.java.training.kovtamas.jdbcpractice.service.impl.ParticipantServiceImpl;
 import hu.schonherz.java.training.kovtamas.jdbcpractice.service.vo.ParticipantVO;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,16 +18,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
         path = "/new",
         method = {RequestMethod.GET, RequestMethod.POST},
         produces = MediaType.TEXT_HTML_VALUE)
-public class NewServlet extends HttpServlet {
+public class ParticipantController extends HttpServlet {
 
-    private final ParticipantService participantService;
+    @Autowired
+    private ParticipantService participantService;
 
-    public NewServlet() {
-        participantService = new ParticipantServiceImpl();
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void save(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         participantService.save(convertToVo(req));
     }
 
